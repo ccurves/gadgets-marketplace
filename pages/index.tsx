e552e1304b5ac62ce097d920aa8ed50ff48423f5
ccurves <simoncovenant47@gmail.com>
2022-11-12 00:50:16 +0100

    Created categories in sanity and finished categories tab

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,16 @@
+import { Tab } from "@headlessui/react";
 import type { GetServerSideProps, NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
 import Header from "../components/Header/Header";
 import Hero from "../components/Hero/Hero";
+import { fetchCategories } from "../utils/fetchFunctions";
 
-const Home: NextPage = () => {
+type Props = {
+  categories: Category[];
+};
+
+const Home: NextPage<Props> = ({ categories }) => {
   return (
     <div className="">
       <Head>
@@ -24,6 +30,31 @@ const Home: NextPage = () => {
           >
             New Promos
           </h1>
+          <Tab.Group>
+            <Tab.List className="flex justify-center">
+              {categories.map((category) => (
+                <Tab
+                  key={category._id}
+                  id={category._id}
+                  className={({ selected }) =>
+                    `whitespace-nowrap rounded-t-lg py-3 px-5 text-sm font-light outline-none md:py-4 md:px-6 md:text-base ${
+                      selected
+                        ? "borderGradient bg-[#35383C] text-white"
+                        : "border-b-2 border-[#35383C] text-[#747474]"
+                    }`
+                  }
+                >
+                  {category.title}
+                </Tab>
+              ))}
+            </Tab.List>
+            {/* <Tab.Panels className="mx-auto max-w-fit pt-10 pb-24 sm:px-4">
+              <Tab.Panel className="tabPanel">{showProducts(0)}</Tab.Panel>
+              <Tab.Panel className="tabPanel">{showProducts(1)}</Tab.Panel>
+              <Tab.Panel className="tabPanel">{showProducts(2)}</Tab.Panel>
+              <Tab.Panel className="tabPanel">{showProducts(3)}</Tab.Panel>
+            </Tab.Panels> */}
+          </Tab.Group>
         </div>
       </section>
     </div>
@@ -32,8 +63,12 @@ const Home: NextPage = () => {
 
 export default Home;
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getServerSideProps: GetServerSideProps<Props> = async () => {
+  const categories = await fetchCategories();
+
   return {
-    props: {},
+    props: {
+      categories,
+    },
   };
 };
