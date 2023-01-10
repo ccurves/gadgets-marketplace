7743b6d86e32a636420dc4d595873b9911dfa2ee
ccurves <simoncovenant47@gmail.com>
2023-01-10 14:12:48 +0100

    Deployed project to vercel

--- pages/api/getSession.ts
+++ pages/api/getSession.ts
@@ -3,16 +3,15 @@ import type { NextApiRequest, NextApiResponse } from "next";
 import Stripe from "stripe";
 const stripe: Stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
-type Data = {
-  name: string;
-};
-
 export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Data>
+  res: NextApiResponse
 ) {
   const sessionId = req.query.session_id as string;
 
   const session = await stripe.checkout.sessions.listLineItems(sessionId);
-  res.status(200).json({ session });
+
+  res.status(200).json({
+    session,
+  });
 }
